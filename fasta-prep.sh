d37742aad6c83bb132efbfe0007b69191677ae82
Brie Flynn <65606659+briannaflynn@users.noreply.github.com>
2021-07-11 13:40:08 -0500

    Add note in comments regarding linux vs mac osx syntax issue

--- fasta-prep.sh
+++ fasta-prep.sh
@@ -5,3 +5,8 @@
 # for linux, remove the -e flag
 var=">SAMPLE"
 sed -i '' -e "1s/.*/$var/" $1_consensus2.fasta
+
+# sed is used here to replace the first line of the $1_consensus2.fasta file with ">SAMPLE"
+
+# for Linux, use this command:
+# sed -i "1s/.*/$var/" $1_consensus2.fasta
