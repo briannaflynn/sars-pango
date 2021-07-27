8bc36412accd3b842d190b4698c538accd05877f
oiiio <gareth862@gmail.com>
2021-07-26 19:12:09 -0500

    updating for PNG

--- toytree-run.sh
+++ toytree-run.sh
@@ -4,6 +4,7 @@ source $2/etc/profile.d/conda.sh
 conda activate bioinformatics
 
 python tree-maker.py $1-results/tree/tree_raw.nwk $1-results/tree/clades.json
-
-echo "If successful tree pdf now available: $1-results/tree/tree-plot.pdf"
-mv tree-plot.pdf $1-results/tree/
+echo "If successful tree pdf now available: $1-results/tree/tree-plot.png"
+mv tree-plot.png $1-results/tree/
+#echo "If successful tree pdf now available: $1-results/tree/tree-plot.pdf"
+#mv tree-plot.pdf $1-results/tree/
