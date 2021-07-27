2bbeb37e2acd754ca9b4a0b54de1f9d3e11f5166
oiiio <gareth862@gmail.com>
2021-07-26 19:49:00 -0500

    more work on preparing PDF

--- manifest/covid.py
+++ manifest/covid.py
@@ -27,7 +27,11 @@ def sortAlleles(genotype):
     #print("INPUT {} SORTER {}".format(genotype, res))
     return res
 
-
+def process_vcf(vcf):
+    """
+    Parse VCF file and run anything necessary to produce the variants table:
+    Dict with position as key and value as dict {'reference': str, 'sample': str}
+    """
 
 def med_json(data, pdf_name, lab_info):
     """
@@ -46,130 +50,10 @@ def med_json(data, pdf_name, lab_info):
     vcf_file = os.path.abspath(results_dir + "/genetic_data/" + sample_name + "_variants.vcf")
     map_file = os.path.abspath(results_dir + "/{}_result.png".format(sample_name))
     tree_file = os.path.abspath(results_dir + "/tree/tree-plot.png")
-    #load drug content so we can add brand names and other info in
-    drug_dir = content_dir + "/drugs"
-    drug_content = {}
-    for i in os.scandir(drug_dir):
-        if i.path.endswith('json') and i.is_file():
-            content = json.load(open(i, "r"))
-            if content['chemName'] in drug_content.keys():
-                logger.error("For some reason drug {} is present as chem name more than once.".format(content['chemName']))
-                raise RuntimeError
-            else:
-                drug_content[content['chemName']] = content
+    #qc_file = os.path.abspath(results_dir + "/qc.json")
+    legend_file = os.path.abspath("manifest/static/vertical_legend.png")
 
-    #prepare medication recommendations
-    drugs = []
-    genetics = {}
-    alerts_count = 0
-    info_count = 0
-    normal_count = 0
-    for rec in data['combined']['drugs']:
-        #check to see if the gene is present in the scope of the order code
-        if data["order_code"] is not None and lab_info.order_codes[data["order_code"]]["genes"] != "full":
-            track = True
-            for gene in rec['genes']:
-                if gene not in lab_info.order_codes[data["order_code"]]["genes"]:
-                    track = False
-            if track is False:
-                continue
-        if rec['guidanceLevel'] == "1":
-            rec['guidanceLevel'] = normal_icon_abspath
-            rec['guidanceInt'] = 1
-            normal_count += 1
-        elif rec['guidanceLevel'] == "2":
-            rec['guidanceLevel'] = info_icon_abspath
-            rec['guidanceInt'] = 2
-            info_count += 1
-        elif rec['guidanceLevel'] == "3":
-            rec['guidanceLevel'] = alert_icon_abspath
-            rec['guidanceInt'] = 3
-            alerts_count += 1
-        else:
-            logger.error("Not a valid guidance level {}".format(rec['guidanceLevel']))
-            raise RuntimeError
 
-        # if brand names become too long, cut them off for the record
-        if len(drug_content[rec['name']]['brandNames']) > 3:
-            branders = drug_content[rec['name']]['brandNames']
-            rec['brand_names'] = ', '.join(branders[0:4])
-        else:
-            rec['brand_names'] = ', '.join(drug_content[rec['name']]['brandNames'])
-
-        rec['brand_names_full'] = ', '.join(drug_content[rec['name']]['brandNames'])
-        # pull out genotypes for the table
-        gts = []
-        for gene in rec['genes']:
-            if "singleton" in data["report_genos"][gene]:
-                for key, val in data["report_genos"][gene].items():
-                    if key != "singleton":
-                        gts.append({key: val})
-                gts.append(data["report_genos"][gene])
-                continue
-            gt = '/'.join(data["report_genos"][gene])
-            gts.append(gt)
-        rec['gts'] = gts
-        rec['genelist'] = ", ".join(rec['genes'])
-        #use the drug content dict to pull in the category
-        rec['category'] = drug_content[rec['name']]['category']
-
-        rec['evidenceLevel'] = re.sub('evidence', '', rec['evidenceLevel'] )
-        drugs.append(rec)
-
-    #prepare disease management recommendations
-    diseases = []
-    for rec in data['combined']['diseases']:
-        # check to see if the gene is present in the scope of the order code
-        if data["order_code"] is not None and lab_info.order_codes[data["order_code"]]["genes"] != "full":
-            track = True
-            for gene in rec['genes']:
-                if gene not in lab_info.order_codes[data["order_code"]]["genes"]:
-                    track = False
-            if track is False:
-                continue
-        if rec['guidanceLevel'] == "1":
-            rec['guidanceLevel'] = normal_icon_abspath
-            rec['guidanceInt'] = 1
-            normal_count += 1
-        elif rec['guidanceLevel'] == "2":
-            rec['guidanceLevel'] = info_icon_abspath
-            rec['guidanceInt'] = 2
-            info_count += 1
-        elif rec['guidanceLevel'] == "3":
-            rec['guidanceLevel'] = alert_icon_abspath
-            rec['guidanceInt'] = 3
-            alerts_count += 1
-        else:
-            logger.error("Not a valid guidance level {}".format(rec['guidanceLevel']))
-            raise RuntimeError
-
-        # pull out genotypes for the table
-        gts = []
-        for gene in rec['genes']:
-            if "singleton" in data["report_genos"][gene]:
-                for key, val in data["report_genos"][gene].items():
-                    if key != "singleton":
-                        gts.append({key: val})
-                gts.append(data["report_genos"][gene])
-                continue
-            gt = '/'.join(data["report_genos"][gene])
-            gts.append(gt)
-        rec['gts'] = gts
-        rec['genelist'] = ", ".join(rec['genes'])
-        rec['evidenceLevel'] = re.sub('evidence', '', rec['evidenceLevel'])
-        diseases.append(rec)
-
-    #prepare the genetics data
-    genetics = prepare_genetics(data, drugs, diseases, lab_info)
-
-    #sort drugs by alphabetical order
-    drugs = sorted(drugs, key=itemgetter('name'))
-    diseases = sorted(diseases, key=itemgetter('name'))
-
-    # load the current medications
-    currents = current_build(data["current_meds"], drugs)
-    current_drugs = currents["current"]
-    medtable = medication_guide(drugs, drug_content)
     templateLoader = jinja2.FileSystemLoader(searchpath="./manifest/static/")
     templateEnv = jinja2.Environment(loader=templateLoader)
     TEMPLATE_FILE = "med_full.html"
@@ -178,7 +62,38 @@ def med_json(data, pdf_name, lab_info):
         title = lab_info.order_codes[data["order_code"]]["title"]
     else:
         title = lab_info.report_title
-    output_html = template.render(css_file=css_abspath, lablogo=logo_abspath, drugs=drugs, first_name=data['first_name'], last_name=data['last_name'], speciment_type=lab_info.specimen_type, collection_date=data["collection_date"], received_date=data["received_date"], report_date=str(date.today().strftime("%m/%d/%Y")), sample_id=data["id"], dob=data["dob"], sex=data["gender"], lab_name=lab_info.address["name"], lab_street=lab_info.address["street"], lab_city=lab_info.address["city"], lab_state=lab_info.address["state"], lab_zip=lab_info.address["zip"], report_title=title, lab_dir=lab_info.clia["lab_dir"], gts=genetics, user_svg=user_abspath, vial_svg=vial_abspath, chartbar_svg=chart_abspath, alert_icon_svg=alert_icon_abspath, info_icon_svg=info_icon_abspath, normal_icon_svg=normal_icon_abspath, current_drugs=current_drugs, medication_guide=medtable, sigpresent=lab_info.signature, diseases=diseases, disclaimer=lab_info.legal["disclaimer"], method=lab_info.legal["methodology"], limitations=lab_info.legal["limitations"], cert=lab_info.legal["labcert"], confident=lab_info.legal["confident"], recipient=lab_info.legal["recipient"], approved=lab_info.legal["approval"])
+    output_html = template.render(css_file=css_abspath,
+                                  lablogo=logo_abspath,
+                                  first_name=data['first_name'],
+                                  last_name=data['last_name'],
+                                  speciment_type=lab_info.specimen_type,
+                                  collection_date=data["collection_date"],
+                                  received_date=data["received_date"],
+                                  report_date=str(date.today().strftime("%m/%d/%Y")),
+                                  sample_id=data["id"],
+                                  dob=data["dob"],
+                                  sex=data["gender"],
+                                  lab_name=lab_info.address["name"],
+                                  lab_street=lab_info.address["street"],
+                                  lab_city=lab_info.address["city"],
+                                  lab_state=lab_info.address["state"],
+                                  lab_zip=lab_info.address["zip"],
+                                  report_title=title,
+                                  lab_dir=lab_info.clia["lab_dir"],
+                                  disclaimer=lab_info.legal["disclaimer"],
+                                  method=lab_info.legal["methodology"],
+                                  limitations=lab_info.legal["limitations"],
+                                  cert=lab_info.legal["labcert"],
+                                  confident=lab_info.legal["confident"],
+                                  recipient=lab_info.legal["recipient"],
+                                  approved=lab_info.legal["approval"],
+                                  qc=data["qc"],
+                                  summary=data["summary"],
+                                  gts=variants,
+                                  tree_path=tree_file,
+                                  legend_path=legend_file,
+                                  map_path=map_file
+                                  )
 
     #write html to /tmp
     logger.debug("writing HTML")
